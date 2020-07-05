9829ed4df063cb2d91f464c36af89274d24d7457
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-07-05 16:19:52 +0900

    settings go on

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const js = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    },
+  },
+};
