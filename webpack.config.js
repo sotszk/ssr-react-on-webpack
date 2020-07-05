cec58fd9e6f223069067576754735db87d730db8
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-07-05 16:37:34 +0900

    add webpack config both server and client

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const js = {
   test: /\.js$/,
   exclude: /node_modules/,
@@ -8,3 +10,39 @@ const js = {
     },
   },
 };
+
+const serverConfig = {
+  mode: 'development',
+  target: 'node',
+  node: {
+    __dirname: false,
+  },
+  externals: [require('webpack-node-externals')()],
+  entry: {
+    'index.js': path.resolve(__dirname, 'src/index.js'),
+  },
+  module: {
+    rules: [js],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]',
+  },
+};
+
+const clientConfig = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    'home.js': path.resolve(__dirname, 'dist/public/home.js'),
+  },
+  module: {
+    rules: [js],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/public'),
+    filename: '[name]',
+  },
+};
+
+module.exports = [serverConfig, clientConfig];
